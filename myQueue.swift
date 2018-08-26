c0404acc1123fb6892fdd4207d6d7a643d353335
superDeano <superdeano@github.com>
2018-08-25 21:00:18 -0400

    new Data Structure
    Added a generic Queue Data Structure!
    Inspired by the Swift Documentation

---
+++ myQueue.swift
@@ -0,0 +1,26 @@
+//
+//  myQueue.swift
+//  FoodTracker
+//
+//  Created by Dean Chong San on 2018-08-25.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import Foundation
+
+struct myQueue <T> {
+    var items = [T]()
+    
+    mutating func enqueue(_ item : T){
+        items.append(item)
+    }
+    
+    mutating func dequeue() -> T {
+        return items.removeFirst()
+    }
+    
+    mutating func isEmpty() -> Bool {
+        return items.isEmpty
+    }
+    
+}
